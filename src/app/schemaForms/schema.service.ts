fffcddbc3a7a99bb7c334d289cd3867db9b5532e
Allan Oliveira <r9oliveira@gmail.com>
2018-06-18 18:37:06 +0800

    Added ability to provide description for Step field via a descriptionUrl property for larger HTML chunks.

--- src/app/schemaForms/schema.service.ts
+++ src/app/schemaForms/schema.service.ts
@@ -12,6 +12,7 @@ export class SchemaService {
           type: 'step',
           title: 'Complaint and Feedback Form',
           description: '<strong>Fill in you details here</strong>',
+          descriptionUrl: 'url-to-override-description.html',
           // showProgress:
           // validateEachStep:
           fields: [
@@ -89,5 +90,24 @@ export class SchemaService {
         }
       ]
     };
+  }
+
+  getHtml(descriptionUrl: string): string {
+    // In future use descriptionUrl to make actual http requests for the HTML
+    return `
+    <div class="instructions">
+      <p>The University welcomes and values your feedback as it assists us to improve our standards and quality of service.  We aim to provide an environment which promotes teamwork and respectful treatment of all staff and students.</p>
+      <ul>
+        <li><strong> <a href="http://complaints.curtin.edu.au/management/advice.cfm" target="_blank">Information  about accessibility</a>, as well as ways to get assistance with making or resolving complaints, is available.</strong></li>
+        <li>Any person is entitled to lodge a complaint with the Curtin University Integrity and Standards Unit (ISU). </li>
+        <li>To lodge a complaint you need to complete this form. The information you provide on this form will be assessed to determine the appropriate course of action.</li>
+        <li>Any information on this form is provided with your consent. </li>
+        <li>In order to address the matter, the details of your complaint will be provided to the appropriate decision maker.  </li>
+        <li>If your complaint is about someone's behaviour it is likely that the appropriate decision maker will provide the details of your complaint to the person identified to allow them to respond. The details of your complaint may also be provided to potential witnesses.</li>
+        <li>After you have submitted this form you will receive a complaint ID number and a copy of your notification by email.</li>
+        <li>If you decide not to provide your contact details we will be unable to provide you with an ID number and a copy of your complaint. Without your contact details we may be unable to progress your complaint.</li>
+        <li>All personal information is handled in accordance with the Australian Privacy Principles.</li>
+      </ul>
+    </div>`;
   }
 }
--- src/app/schemaForms/sf-step/sf-step.component.ts
+++ src/app/schemaForms/sf-step/sf-step.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { SchemaService } from '../schema.service';
 
 @Component({
   selector: 'sf-step',
@@ -10,9 +11,19 @@ export class SfStepComponent implements OnInit {
   @Input() fields: any[];
   @Input() form: FormGroup;
 
-  constructor() { }
+  constructor(private schemaService: SchemaService) {}
 
   ngOnInit() {
+    if(this.fields.length > 0) {
+      this.setDescriptions();
+    }
   }
 
+  setDescriptions() : void {
+    for(var i = 0; i < this.fields.length; i++) {
+      if(this.fields[i].type === 'step' && this.fields[i].descriptionUrl) {
+        this.fields[i].description = this.schemaService.getHtml(this.fields[i].descriptionUrl);
+      }
+    }
+  }
 }
